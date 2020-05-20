9688af9e54adaa369093e29252abed10ab941b62
CCulver831 <c.culver831@gmail.com>
2020-05-19 22:18:38 -0700

    Added Coordinate script

---
+++ Location/Assets/Scripts/Coord.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coord
+{
+    float X;
+    float Y;
+    float Z;
+
+    public Coord(float x, float y)
+    {
+        X = x;
+        Y = y;
+        Z = -1;
+    }
+
+    public override string ToString()
+    {
+        return "(" + X + " , " + Y + ", " + Z + ")";
+    }
+}
